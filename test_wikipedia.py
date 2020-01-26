1cff2eeb21e3bf45b34a312f48bbd8711d89b205
Truman Smith <trumanwsmith@gmail.com>
2020-01-25 19:30:07 -0800

    Refactors current events tests to pass CurrentEventsPage object to methods instead of create new object.

--- test_wikipedia.py
+++ test_wikipedia.py
@@ -324,9 +324,9 @@ class TestCurrentEventsPage(WikipediaCommon):
 
 	#@unittest.skip('')
 	def test_main_current_events_page(self):
-		self.navigate_to_current_events_page()
+		ce_page = self.navigate_to_current_events_page()
 		now = datetime.now()
-		self.verify_date_headers(
+		self.verify_date_headers(ce_page,
 			now.strftime('%B'), now.strftime('%Y'), days_ascending=False)
 
 	#@unittest.skip('')
@@ -334,48 +334,55 @@ class TestCurrentEventsPage(WikipediaCommon):
 		if browser == "safari":
 			self.skipTest('Safari does not locate month_year link')
 
-		self.navigate_to_current_events_page()
-		month, year = self.select_random_month_year()
-		self.verify_date_headers(month, year, days_ascending=True)
+		ce_page = self.navigate_to_current_events_page()
+		month, year = self.select_random_month_year(ce_page)
+		self.verify_date_headers(ce_page, month, year, days_ascending=True)
 
 	def test_archived_months_link_text(self):
-		self.navigate_to_current_events_page()
-		self.verify_archives_link_text()
+		ce_page = self.navigate_to_current_events_page()
+		self.verify_archives_link_text(ce_page)
 
 	####################
 	# Helper functions
 	####################
 
+	# Click link to Current Events on left panel
+	#   returns an CurrentEvents page object
 	def navigate_to_current_events_page(self):
 		self.main = pages.MainPage(self.driver)
 		self.main.open_main_page()
 		self.main.click_left_panel_link("Current events")
+		return pages.CurrentEventsPage(self.driver)
 
 	# Randomly select a month from the archives at the bottom of the page
 	#   Earliest expected archive is July 1994
 	#   Latest expected archive is the current month
-	# Returns the tuple (month, year) which was selected
-	def select_random_month_year(self):
-		self.ce = pages.CurrentEventsPage(self.driver)
-		year = random.randint(self.ce.first_archived_year, datetime.now().year)
+	# parameter
+	#   ce_page - CurrentEvents page object
+	# returns
+	#   the tuple (month, year) which was selected
+	def select_random_month_year(self, ce_page):
+		year = random.randint(ce_page.first_archived_year, datetime.now().year)
 
-		first_month, last_month = self.ce.get_month_range(year)
-		month = self.ce.month_name(random.randint(first_month, last_month))
+		first_month, last_month = ce_page.get_month_range(year)
+		month = ce_page.month_name(random.randint(first_month, last_month))
 
 		print("Verifying {} {}".format(month, year))
-		self.ce.click_link_archived_month(month, year)
+		ce_page.click_link_archived_month(month, year)
 		return (month, year)
 
 	# Verify link text on the archived months
-	# Expect to find links July 1994 to the current month
-	def verify_archives_link_text(self):
+	# parameter
+	#   ce_page - CurrentEventsPage object
+	# Expects groups of links from the current year to the first archived year
+	#   each group has one link for the year and links for each month
+	def verify_archives_link_text(self, ce_page):
 		current_year = datetime.now().year
-		self.ce = pages.CurrentEventsPage(self.driver)
 
-		years = self.ce.get_archive_links_by_year()
+		years = ce_page.get_archive_links_by_year()
 		for yr in years:
 
-			links = self.ce.parse_archive_links(yr)
+			links = ce_page.parse_archive_links(yr)
 
 			# verify the first link is the year
 			yr_str = str(current_year)
@@ -386,11 +393,11 @@ class TestCurrentEventsPage(WikipediaCommon):
 			# verify the remaining links are months
 			#   while most years will be months# 1-12
 			#   the first archived and current year will be fewer months
-			first_month, last_month = self.ce.get_month_range(current_year)
+			first_month, last_month = ce_page.get_month_range(current_year)
 			current_month = first_month
 
 			for i in range(1, len(links)):
-				month_name = self.ce.month_name(current_month)
+				month_name = ce_page.month_name(current_month)
 
 				self.assertEqual(links[i]["text"], month_name)
 				regex = ".*{} {}$".format(month_name, yr_str)
@@ -407,22 +414,22 @@ class TestCurrentEventsPage(WikipediaCommon):
 	#   verify headers are the expected format (ex: Janurary 1, 1999 (Monday))
 	#   verify dates are in sequence
 	# Parameters
+	#   ce_page - CurrentEventsPage object
 	#   month - string, full spelling of a month
 	#   year - string, YYYY format
 	#   days_ascending - boolean, dates should be in ascending order if True
 	#                    in descending order if False
-	def verify_date_headers(self, month, year, days_ascending=True):
-		ce = pages.CurrentEventsPage(self.driver)
-		dates = ce.get_date_headers()
+	def verify_date_headers(self, ce_page, month, year, days_ascending=True):
+		dates = ce_page.get_date_headers()
 
 		days = []
 		for date in dates:
-			self.assertRegex(date, ce.long_date_regex)  # header is expected format
-			date_parsed = ce.parse_date_header(date)
+			self.assertRegex(date, ce_page.long_date_regex)  # header is expected format
+			date_parsed = ce_page.parse_date_header(date)
 			self.assertEqual(month, date_parsed[0])     # expected month
 			self.assertEqual(year, date_parsed[2])      # expected year
 			days.append( (int(date_parsed[2]),
-				          ce.month_index(date_parsed[0]), 
+				          ce_page.month_index(date_parsed[0]), 
 				          int(date_parsed[1])) )	
 		# days are in expected sequence
 		self.assertEqual(days, sorted(days, reverse=(not days_ascending)))
